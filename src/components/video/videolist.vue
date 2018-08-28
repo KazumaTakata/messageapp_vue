8bd81285204803082f7705e0e6a6a41952a3de9d
Kazuma Takata <neilbuzztakata@gmail.com>
2018-08-29 01:28:04 +0900

    video viewer with controller

--- src/components/video/videolist.vue
+++ src/components/video/videolist.vue
@@ -2,32 +2,58 @@
     <div>
         <ul>
             <li v-on:click="videoopen" v-for="(item, index) in videolist" v-bind:key="index" v-bind:id="index">
-
                 <div>
                     <img class="profile-img" v-bind:src="getphoto(item.friendid)"> {{getname(item.friendid)}}
                 </div>
                 <div>
                     {{item.time}}
                 </div>
-                <template v-if=" String( whichvideo ) === String( index )">
-                    <div class="video__container">
-                        <button v-on:click="videoclose" class="closebutton">
-                            <font-awesome-icon icon="times" />
-                        </button>
-                        <div class="inner__video__container">
-                            <video id="gum" v-bind:src="item.video_local" playsinline></video>
-                            <video id="gum" v-bind:src="item.video_remote" playsinline></video>
-                        </div>
-                        <div class="video__comment">
-                            Note
-                        </div>
-                        <div>
-                            ofiewjfoewjfoewajoifejwa
-                        </div>
-                    </div>
-                </template>
+
             </li>
         </ul>
+        <template v-if=" videotoggle ">
+            <div class="video__container">
+                <button v-on:click="videoclose" class="closebutton">
+                    <font-awesome-icon icon="times" />
+                </button>
+                <div class="inner__video__container">
+                    <video ref="video_local" id="gum" v-bind:src="videolist[activeindex].video_local"></video>
+                    <video ref="video_remote" id="gum" v-bind:src="videolist[activeindex].video_remote"></video>
+                </div>
+                <ul id="video-controls" class="controls">
+                    <li>
+                        <button id="playpause" v-on:click="playbutton" type="button">Play/Pause</button>
+                    </li>
+                    <li>
+                        <button id="stop" v-on:click="stopbutton" type="button">Stop</button>
+                    </li>
+                    <li class="progress">
+                        <progress ref="progress" id="progress" value="0" min="0">
+                            <span ref="progressbar" id="progress-bar"></span>
+                        </progress>
+                    </li>
+                    <!-- <li>
+                        <button id="mute" type="button">Mute/Unmute</button>
+                    </li> -->
+                    <li>
+                        <button id="volinc" type="button">Vol+</button>
+                    </li>
+                    <li>
+                        <button id="voldec" type="button">Vol-</button>
+                    </li>
+                    <!-- <li>
+                        <button id="fs" type="button">Fullscreen</button>
+                    </li> -->
+                </ul>
+
+                <div class="video__comment">
+                    Note
+                </div>
+                <div>
+                    ofiewjfoewjfoewajoifejwa
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -38,19 +64,63 @@ export default {
   data() {
     return {
       videolist: [],
-      whichvideo: ''
+      whichvideo: '',
+      videotoggle: false,
+      activeindex: 0
     }
   },
   created() {
     this.getVideoList()
+
+    // this.$refs.video_local.ontimeupdate = () => {
+    //   this.$refs.progress.value = this.$refs.video_local.currentTime
+    //   this.$refs.progressbar.style.width =
+    //     Math.floor(
+    //       this.$refs.video_local.currentTime /
+    //         this.$refs.video_local.duration *
+    //         100
+    //     ) + '%'
+    // }
   },
+  mounted: function() {},
   methods: {
+    progressbarinit: function(event) {
+      this.$refs.video_local.ontimeupdate = () => {
+        try {
+          this.$refs.progress.max = this.$refs.video_local.duration
+          this.$refs.progress.value = this.$refs.video_local.currentTime
+        } catch (err) {
+          console.log(err)
+        }
+
+        this.$refs.progressbar.style.width =
+          Math.floor(
+            this.$refs.video_local.currentTime /
+              this.$refs.video_local.duration *
+              100
+          ) + '%'
+      }
+    },
+
+    playbutton: function(event) {
+      this.progressbarinit()
+      console.log(event)
+      this.$refs.video_local.play()
+      this.$refs.video_remote.play()
+    },
+    stopbutton: function(event) {
+      this.$refs.video_local.pause()
+      this.$refs.video_remote.pause()
+    },
     videoopen: function(event) {
       //   console.log(event.target.id)
-      this.whichvideo = String(event.target.id)
+      this.videotoggle = true
+      this.activeindex = Number(event.target.id)
+      //   this.whichvideo = String(event.target.id)
     },
     videoclose: function(event) {
       console.log(event.target.id)
+      this.videotoggle = false
       this.whichvideo = ''
     },
     getVideoList: async function(event) {
@@ -92,10 +162,33 @@ li {
 li * {
   pointer-events: none;
 }
+ul {
+  list-style: none;
+}
 
 li:hover {
   background: $hover-color;
   //   color: white;
+}
+
+.controls {
+  list-style: none;
+  display: flex;
+
+  .progress {
+    width: 50%;
+  }
+}
+
+progress span {
+  width: 0%;
+  height: 100%;
+  display: inline-block;
+  background-color: #2a84cd;
+}
+
+progress {
+  width: 100%;
 }
 
 .video__container {
@@ -108,6 +201,9 @@ li:hover {
   z-index: 10;
   color: white;
 
+  button {
+    pointer-events: auto;
+  }
   .inner__video__container {
     display: flex;
 
